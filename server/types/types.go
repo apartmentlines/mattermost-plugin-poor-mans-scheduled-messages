2c2c57319e1b0cf97fa1c3c11b39aa3932905e71
Chad Phillips <chad@apartmentlines.com>
2025-04-17 15:47:54 -0400

    add Logger interface

--- server/types/types.go
+++ server/types/types.go
@@ -2,6 +2,13 @@ package types
 
 import "time"
 
+type Logger interface {
+	Error(msg string, keyvals ...any)
+	Warn(msg string, keyvals ...any)
+	Info(msg string, keyvals ...any)
+	Debug(msg string, keyvals ...any)
+}
+
 type ScheduledMessage struct {
 	ID             string    `json:"id"`
 	UserID         string    `json:"user_id"`
